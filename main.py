863ece7bef1f554fe3e7cc13fa93043eaa888c82
DENN87 <denis.schiopu@yahoo.com>
2022-03-04 01:34:48 -0800

    Implemented the contact.html and contact form.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+# for sending emails
+import smtplib
 
 app = Flask(__name__)
 
@@ -18,14 +20,28 @@ def get_blog(post_id):
     return render_template('blog.html', blog=blogs_data[post_id-1])
 
 
+@app.route('/compose')
+def compose_post():
+    return render_template('compose.html')
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    if request.method == 'GET':
+        # return for GET
+        return render_template('contact.html', msg_sent=False)
+    else:
+        # return for POST
+        form_name = request.form['userName']
+        form_email = request.form['userEmail']
+        form_text = request.form['userText']
+        # print(form_name, form_email, form_text)
+        return render_template('contact.html', msg_sent=True)
 
 
 if __name__ == "__main__":
