adb419bf0218f47d4efcef7d35c0872376bb2feb
DENN87 <denis.schiopu@yahoo.com>
2022-03-04 02:03:32 -0800

    Implemented Contact ME form to be sent to a Gmail account.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import os
+
 from flask import Flask, render_template, request
 import requests
+from dotenv import load_dotenv
 # for sending emails
 import smtplib
+
+# take environment variables from .env.
+load_dotenv()
+OWN_EMAIL = os.environ.get('OWN_EMAIL')
+OWN_PASSWORD = os.environ.get('OWN_PASSWORD')
 
 app = Flask(__name__)
 
@@ -17,7 +25,7 @@ def home():
 
 @app.route('/blog/<int:post_id>')
 def get_blog(post_id):
-    return render_template('blog.html', blog=blogs_data[post_id-1])
+    return render_template('blog.html', blog=blogs_data[post_id - 1])
 
 
 @app.route('/compose')
@@ -41,7 +49,16 @@ def contact():
         form_email = request.form['userEmail']
         form_text = request.form['userText']
         # print(form_name, form_email, form_text)
+        send_email(form_name, form_email, form_text)
         return render_template('contact.html', msg_sent=True)
+
+
+def send_email(name, email, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nMessage: {message}"
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 
 if __name__ == "__main__":
