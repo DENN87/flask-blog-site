cccdc4fbf611c22c15643c710a37aca9b0ff8477
DENN87 <denis.schiopu@yahoo.com>
2022-03-03 23:49:11 -0800

    Created routes for each blog post by id, refactored HTML files.

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import requests
 
 app = Flask(__name__)
 
+api_url = 'https://api.npoint.io/38823c0454884cd19c9c'
+data_response = requests.get(api_url)
+blogs_data = data_response.json()
+
 
 @app.route('/')
 def home():
-    api_url = 'https://api.npoint.io/38823c0454884cd19c9c'
-    data_response = requests.get(api_url)
-    blogs_data = data_response.json()
-    print(blogs_data)
     return render_template('home.html', blogs=blogs_data)
+
+
+@app.route('/blog/<int:post_id>')
+def get_blog(post_id):
+    return render_template('blog.html', blog=blogs_data[post_id-1])
 
 
 @app.route('/about')
