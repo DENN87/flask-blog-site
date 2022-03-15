ba6d9f7ab070e96efbe9e864e2eb36998e9110a6
DENN87 <denis.schiopu@yahoo.com>
2022-03-15 11:32:51 -0700

    Added view posts by Author name. Implemented author route.

--- flaskr/authors.py
+++ flaskr/authors.py
@@ -11,3 +11,14 @@ def authors():
         'SELECT username FROM user',
     ).fetchall()
     return render_template('authors.html', authors=authors)
+
+
+@bp.route('/authors/<string:username>')
+def authors_by_username(username):
+    db = get_db()
+    posts_by_username = db.execute(
+        'SELECT p.id, title, body, created, likes, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE u.username = ?', (username,)
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts_by_username)
